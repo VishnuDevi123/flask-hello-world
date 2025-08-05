3cfc093f924bca57b20e3908ff2439b9a55dc229
Vishnu Devireddy <vishnudevireddy@Vishnus-MacBook-Pro.local>
2025-08-05 00:34:38 -0700

    Initial Commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello World and Welcome to Flask!"
+    return "Hello World"
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0' , port = 5000, debug=True)
